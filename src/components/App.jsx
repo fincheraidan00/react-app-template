2002d0b7de4c263d1eba8e74ca1f3581f38b69c2
Aidan Fincher <aidan.m.fincher@gmail.com>
2021-03-25 09:09:26 -0500

    Updated the 'Format' command to cover all subdirectories in the src folder rather than just the root level. Changed the order in which ESLint styles were being applied in order to fix conflict between ESLint and Semistandard style guide.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return (
-      <h1>React App Working</h1>
-    );
+    return <h1>React App Working</h1>;
   }
 }
 
