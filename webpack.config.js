10d47ac4f2395903e381088629d3f446868abba3
Aidan Fincher <aidan.m.fincher@gmail.com>
2021-03-25 09:15:04 -0500

    Minor changes to webpack.config.js. Added commets throughout and matched style to standards used throughout the rest of the project. Final version of this template for now. This template will be built into full MERN-stack template eventually.

--- webpack.config.js
+++ webpack.config.js
@@ -6,22 +6,34 @@ module.exports = {
   mode: 'development',
   // Set the app entry point to ./src/index.js
   entry: path.resolve(__dirname, 'src', 'index.js'),
+  // Configure build parameters including output directory and output filename
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
+  /**
+   * Configure build rules for module.
+   * This checks for JSX files, excludes the /node_modules/ directory from the build,
+   * and sets the babel presets to be used in the project.
+   */
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
-      },
-    }],
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
   },
+  // Ensures that webpack can detect all filetypes, not just vanilla JavaScript source files.
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
-  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })],
+  // Add the HtmlWebpackPlugin to the build, and specify which html file to bundle with.
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })
+  ]
 };
