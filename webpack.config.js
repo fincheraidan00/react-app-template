c4ed2f947e5ecb271896984fa816c429dbf8b430
Aidan Fincher <aidan.m.fincher@gmail.com>
2021-03-25 05:51:55 -0500

    Basic template for React.js application. This template will probably not have additonal pushes outside of adding production build system.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // Set development mode to 'DEVELOPMENT' change to 'PRODUCTION' for release
+  mode: 'development',
+  // Set the app entry point to ./src/index.js
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    //
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: ["@babel/preset-env", "@babel/preset-react"]
+      }
+    }]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })]
+};
